a23bc8aab05b5edcbc1e28fa365c4b4582e344ba
Moacir Costa <moacircostajr@gmail.com>
2022-02-11 11:01:58 -0300

    feat: first template use

--- app/app/core/views.py
+++ app/app/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html', {'usuario': 'Fulano de Tal'})
+    # return HttpResponse('Hello World')
