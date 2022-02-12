2ad162716c4e7268e162ad8b95e5caebeca96ea3
Moacir Costa <moacircostajr@gmail.com>
2022-02-11 21:33:40 -0300

    feat: custom filtering manager

--- app/app/courses/models.py
+++ app/app/courses/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class CourseManager(models.Manager):
+    def search(self, query):
+        return self.get_queryset().filter(
+            # name__icontains=query, description__icontains=query  # AND  -> models.Q(name__icontains=query) & models.Q(description__icontains=query)
+            models.Q(name__icontains=query) | models.Q(description__icontains=query)  # OR
+            )
 
 class Course(models.Model):
     name = models.CharField('Nome', max_length=100)
@@ -12,3 +18,5 @@ class Course(models.Model):
         upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)  # MEDIA_ROOT = em settings.py
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    objects = CourseManager()
