a45eb492e3f78437bb9bd2fba65e37fe40a7edd3
Moacir Costa <moacircostajr@gmail.com>
2022-02-12 08:15:10 -0300

    feat: show course details

--- app/app/courses/urls.py
+++ app/app/courses/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'courses'  # namespace
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pk>/detalhes', views.details, name='details'),  # https://docs.djangoproject.com/en/3.2/topics/http/urls/
 ]
--- app/app/courses/views.py
+++ app/app/courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 # Create your views here.
 
@@ -7,3 +7,9 @@ def index(request):
     courses = Course.objects.all()
     context = {'courses': courses}
     return render(request, 'courses.html', context)
+
+
+def details(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    context = {'course': course}
+    return render(request, 'details.html', context)
